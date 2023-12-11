ecab620909d46409141521eb88a307d5b35cf321
WSH032 <614337162@qq.com>
2023-12-11 09:37:18 +0800

    feat: add python api `main`

--- src/aria2c/__init__.py
+++ src/aria2c/__init__.py
@@ -12,7 +12,7 @@ import signal
 import sys
 from pathlib import Path
 from subprocess import Popen
-from typing import Any, NoReturn
+from typing import Any, Iterable, NoReturn, Optional
 
 __all__ = ("ARIA2C", "main")
 
@@ -21,6 +21,7 @@ CWD = Path(__file__).parent.absolute()
 # NOTE: keep the `bin dir` consistent with the one in `hatch_build.py`
 BIN = CWD / "bin"
 ARIA2C = BIN / ("aria2c.exe" if sys.platform == "win32" else "aria2c")
+"""aria2c binary executable file path."""
 
 
 CONSOLE_SIGNALS = (signal.SIGINT,)  # Unix signal 2. Sent by Ctrl+C.
@@ -35,8 +36,19 @@ CLI_SIGNALS = (signal.SIGTERM,)  # Unix signal 15. Sent by `kill <pid>`.
 _logger = logging.info
 
 
-def main() -> NoReturn:
-    """Launch aria2c as a subprocess."""
+def main(argv: Optional[Iterable[str]] = None) -> NoReturn:
+    """Launch aria2c as a subprocess.
+
+    Args:
+        argv: The command line arguments that will be passed to aria2c.
+            If `None`, `sys.argv[1:]` will be used.
+
+    Raises:
+        RuntimeError: If aria2c binary not found.
+
+    Returns:
+        Never return, always call `sys.exit()` with the same exit code as aria2c subprocess.
+    """
     if not ARIA2C.is_file():
         raise RuntimeError(f"aria2c binary not found: {ARIA2C}")
 
@@ -45,9 +57,10 @@ def main() -> NoReturn:
     for sig in CONSOLE_SIGNALS:
         signal.signal(sig, signal.getsignal(signal.SIGINT))
 
-    argv = sys.argv[1:]
+    if argv is None:
+        argv = sys.argv[1:]
 
-    _logger(argv)
+    _logger(f"launch argv: {argv}")
     _logger(f"python pid: {os.getpid()}")
     with Popen([str(ARIA2C), *argv]) as aria2_subprocess:
         _logger(f"aria2c pid: {aria2_subprocess.pid}")
